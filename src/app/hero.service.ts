74a157285414a4bbdf10db15fb03984c9cbc60be
john-emerson14 <john.carv.sousa@gmail.com>
2019-12-30 19:34:33 -0300

    feat: get heroes with httpclient

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -15,9 +15,9 @@ export class HeroService {
   constructor(private http: HttpClient,
               private messageService: MessageService) { }
 
-  getHeroes(): Observable<Hero[]> {
-    this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
+  /** GET heroes from the server */
+  getHeroes (): Observable<Hero[]> {
+    return this.http.get<Hero[]>(this.heroesUrl)
   }
 
   getHero(id: number): Observable<Hero> {
